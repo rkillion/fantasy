366ba9651b63876fbb318a29e26eaf02057e57d3
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-06-28 17:19:50 -0400

    Grid Map Updates

--- code.js
+++ code.js
@@ -1,94 +1,57 @@
-getPyramid(10,100);
-
 const mySetting = {title: "settingTitle",worlds: []}; //an array of Worlds with their populations
 const basePopulation = 100000000;
 const basePantheon = 110;
 const world = {title: "worldTitle",population: basePopulation,continents: [],tags: []}
 const continent = {title: "continentTitle",population: 0,kingdoms: [],linkedTags: [],tags: []}
 const mapTypes = ["world","continent","subcontinent","kingdom","province"]
+const gridMapFrameHeight = 601;
+const gridMapFrameWidth = 1201;
 
-let primaryWorld = {title: "primaryWorld", type: "world",population: 100000000, continent: [],tags: ["primaryWorld","materialPlane"]}; //in the future, I should call the submaps "submaps" with a mapType property, rather than naming each one differently
-primaryWorld.continent[0] = {title: "Capalon", population: 3630424, assigned: true}
-primaryWorld.continent[1] = {title: "continent1", population: 8363473, assigned: false}
-primaryWorld.continent[2] = {title: "continent2", population: 5975046, assigned: false}
-primaryWorld.continent[3] = {title: "continent3", population: 7911199, assigned: false}
-primaryWorld.continent[4] = {title: "continent4", population: 3631948, assigned: false}
-primaryWorld.continent[5] = {title: "continent5", population: 4495935, assigned: false}
-primaryWorld.continent[6] = {title: "continent6", population: 7403132, assigned: false}
-primaryWorld.continent[7] = {title: "Elmanor", population: 19136631, assigned: true}
-primaryWorld.continent[8] = {title: "continent8", population: 4048694, assigned: false}
-primaryWorld.continent[9] = {title: "continent9", population: 35403518, assigned: true}
+let gridUnitChangeColor = "lightblue";
 
-let capalon = {title:"Capalon",type: "continent",population:3630424,subcontinent:[{title:"kingdom0",population:123854,assigned:false},{title:"kingdom1",population:550074,assigned:false},{title:"kingdom2",population:338225,assigned:false},{title:"kingdom3",population:6407,assigned:false},{title:"kingdom4",population:137713,assigned:false},{title:"kingdom5",population:54169,assigned:false},{title:"kingdom6",population:385251,assigned:false},{title:"kingdom7",population:589219,assigned:false},{title:"kingdom8",population:209784,assigned:false},{title:"kingdom9",population:96837,assigned:false},{title:"kingdom10",population:336755,assigned:false},{title:"kingdom11",population:591916,assigned:false},{title:"kingdom12",population:210220,assigned:false}],linkedTags: primaryWorld.tags,tags:["Capalon"]}
-let elmanor = {title:"Elmanor",type: "continent",population:19136631,subcontinent:[{title:"kingdom0",population:70944,assigned:false},{title:"kingdom1",population:2739588,assigned:false},{title:"kingdom2",population:2746923,assigned:false},{title:"kingdom3",population:201318,assigned:false},{title:"kingdom4",population:1826820,assigned:false},{title:"kingdom5",population:1925773,assigned:false},{title:"kingdom6",population:508181,assigned:false},{title:"kingdom7",population:2992805,assigned:false},{title:"kingdom8",population:859184,assigned:false},{title:"kingdom9",population:2188972,assigned:false},{title:"kingdom10",population:1266693,assigned:false},{title:"kingdom11",population:193909,assigned:false},{title:"kingdom12",population:1615521,assigned:false}],linkedTags:["primaryWorld","materialPlane"],tags:["Elmanor"]}
+let places = {};
+let placeHandles;
 
-let places = {}//{mainWorld: primaryWorld,firstContinent: elmanor,test: "The test worked."};
-
-function createWorld(name,population=0,numOfContinents=0) {
-    let newWorld = {};
-    newWorld.title = name;
-    newWorld.population = population || basePopulation;
-    continentArray= [];
-    continentCount = numOfContinents || randomIntBetween(8,20);
-    let remainingPopulation = newWorld.population;
-    for (let i=0;i<continentCount;i++) {
-        let thisContinent = {};
-        thisContinent.title = `continent${i}`;
-        if (i===continentCount-1){
-            thisContinent.population = remainingPopulation;
-        } else {
-            thisContinent.population = Math.round(remainingPopulation/((continentCount-i)/2) * Math.random());
-        }
-        remainingPopulation = remainingPopulation-thisContinent.population;
-        continentArray = [...continentArray,thisContinent];
-    }
-    newWorld.continent = continentArray;
-    console.log(JSON.stringify(newWorld));
-    return newWorld;
+//create a function paired with a button on the page, then you can write your code in that function and run it after you load the places
+function runButtonFunction() {
+    generateGrid();
 }
 
-function createContinent(name,world,existingContinentNum,numOfKingdoms=0) {
-    let newContinent = {};
-    newContinent.title = name;
-    newContinent.population = world.continents[existingContinentNum].population;
-    kingdomArray= [];
-    kingdomCount = numOfKingdoms || randomIntBetween(6,14);
-    let remainingPopulation = newContinent.population;
-    for (let i=0;i<kingdomCount;i++) {
-        let thisKingdom = {};
-        thisKingdom.title = `kingdom${i}`;
-        if (i===kingdomCount-1){
-            thisKingdom.population = remainingPopulation;
-        } else {
-            thisKingdom.population = Math.round(remainingPopulation/((kingdomCount-i)/2) * Math.random());
-        }
-        remainingPopulation = remainingPopulation-thisKingdom.population;
-        thisKingdom.assigned = false;
-        kingdomArray = [...kingdomArray,thisKingdom];
-    }
-    newContinent.kingdoms = kingdomArray;
-    newContinent.linkedTags = world.tags;
-    newContinent.tags = [name];
-    world.continents[existingContinentNum].title = name;
-    world.continents[existingContinentNum].assigned = true;
-    console.log(JSON.stringify(newContinent));
-    return newContinent;
-}
-
-function createPlace(name,numOfSubMaps=0,population=basePopulation,parentMap="",placeReference="") { //place reference is the array index of the submap that you want to use
+function createPlace(name,handle,numOfSubMaps=0,population=basePopulation,parentMap="",placeReference="",tags=[]) { //place reference is the array index of the submap that you want to use
     let newPlace = {};
     newPlace.title = name;
+    if(parentMap) {newPlace.parentMap = parentMap.title};
     newPlace.type = parentMap ? mapTypes[mapTypes.indexOf(parentMap.type)+1] : "world";
     let thisPlaceReference; 
-    if (placeReference) {
+    if (placeReference || newPlace.type==="world") {
         thisPlaceReference = placeReference;
     } else {
-        let freePlaces = filterPlaces(parentMap[newPlace.type],"assigned",false);
+        let freePlaces = filterPlaces(parentMap.subMap,"assigned",false);
         let highestReference = searchPopulation(freePlaces,"greatest");
-        placeReference = freePlaces[highestReference].title.slice(-1);
+        thisPlaceReference = freePlaces[highestReference].title.slice(-1);
     }
-    newPlace.population = newPlace.type==="world" ? population : parentMap[newPlace.type][placeReference].population;
-    //need to add the code to create the submaps and tags, then be sure to add the code that changes the submap title and assigned status on the parent map
+    newPlace.population = newPlace.type==="world" ? population : parentMap.subMap[thisPlaceReference].population;
+    let submapArray = [];
+    subMapCount = numOfSubMaps || randomIntBetween(6,18);
+    let remainingPopulation = newPlace.population;
+    for (let i=0;i<subMapCount;i++) {
+        let thisSubMap = {};
+        thisSubMap.title = `${mapTypes[mapTypes.indexOf(newPlace.type)+1]}${i}`;
+        thisSubMap.type = mapTypes[mapTypes.indexOf(parentMap.type)+1]
+        if (i===subMapCount-1){
+            thisSubMap.population = remainingPopulation;
+        } else {
+            thisSubMap.population = Math.round(remainingPopulation/((subMapCount-i)/2) * Math.random());
+        }
+        remainingPopulation = remainingPopulation-thisSubMap.population;
+        thisSubMap.assigned = false;
+        submapArray = [...submapArray,thisSubMap];
+    }
+    newPlace.subMap = submapArray;
+    newPlace.tags = [...tags,name,handle];
+    if(parentMap) {parentMap.subMap[thisPlaceReference].title = newPlace.title;}
+    if(parentMap) {parentMap.subMap[thisPlaceReference].assigned = true;}
+    places = {...places,[handle]: newPlace};
     return newPlace;
 }
 
@@ -145,7 +108,84 @@ function subMapDimensionsFromGenerated(map,percentToUse,height=2000,width=4000,s
     return Math.round((width*height)*(percentToUse/100)*(1/map[subMapType].length))
 }
 
+//---------GRID MAP
+let changeColorOnMouseOver = false;
 
+function generateGrid() {
+    for (let p=1;p<=60;p++){
+    for (let i=1;i<=120;i++) {
+    let gridUnit = document.createElement("div");
+    gridUnit.id = `${p}-${i}`;
+    gridUnit.style.left=`${(i-1)*10}px`;
+    gridUnit.style.top=`${(p-1)*10}px`;
+    gridUnit.style.display= "block";
+    gridUnit.style.position="absolute";
+    gridUnit.style.border = "1px solid grey";
+    gridUnit.style.background = "lightblue";
+    gridUnit.style.borderLeft = "0px";
+    gridUnit.style.borderTop = "0px";
+    gridUnit.style.height = "9px";
+    gridUnit.style.width = "9px";
+    gridUnit.addEventListener("mousedown", function() {changeColorOnMouseOver=true});
+    gridUnit.addEventListener("mouseup",function() {changeColorOnMouseOver=false});
+    gridUnit.addEventListener("mouseover",mouseoverGridBoxColor);
+    gridUnit.addEventListener("click",changeGridBoxColor);
+    document.getElementById("gridMapFrame").appendChild(gridUnit);
+    }}
+}
+
+function changeGridBoxColor(event) {
+    let thisGridBox = event.target;
+    thisGridBox.style.background = gridUnitChangeColor;
+}
+
+function mouseoverGridBoxColor(event) {
+    if(changeColorOnMouseOver) {
+        let thisGridBox = event.target;
+        thisGridBox.style.background = gridUnitChangeColor;
+    }
+}
+
+function loadMap() {
+    let map = document.getElementById("mapHandleInput").value;
+    for (let p=1;p<=60;p++){
+        for (let i=1;i<=120;i++) {
+            let thisGridUnit = document.getElementById(`${p}-${i}`);
+            thisGridUnit.style.background = places[map].grid[`${p}-${i}`];
+        }
+    }
+}
+
+function saveMap() {
+    let map = document.getElementById("mapHandleInput").value;
+    let thisMap = {};
+    for (let p=1;p<=60;p++){
+        for (let i=1;i<=120;i++) {
+            let thisGridUnit = document.getElementById(`${p}-${i}`);
+            thisMap[`${p}-${i}`] = thisGridUnit.style.background;
+        }
+    }
+    places[map].grid = thisMap;
+}
+
+function drawSea() {
+    clearButtons();
+    gridUnitChangeColor = "lightblue";
+    document.getElementById("seaButton").style.background = "lightgreen";
+}
+
+function drawLand() {
+    clearButtons();
+    gridUnitChangeColor = "green";
+    document.getElementById("landButton").style.background = "lightgreen";
+}
+
+function clearButtons() {
+    let drawButtons = document.getElementsByClassName("drawButton");
+    for (const element of drawButtons) {
+        element.style.background = "lightcyan";
+    }
+}
 //---------FILE SAVER
 
 
@@ -161,6 +201,7 @@ var reader; //GLOBAL File Reader object for demo purpose only
 * Check for the various File API support.
 */
 function checkFileAPI() {
+    generateGrid();
 if (window.File && window.FileReader && window.FileList && window.Blob) {
     reader = new FileReader();
     return true; 
@@ -204,7 +245,108 @@ return true;
 }   
 
 function loadPlaces(txt) {
-    places = JSON.parse(txt)
+    places = JSON.parse(txt);
+    placeHandles = Object.keys(places);
+    autocomplete(document.getElementById("mapHandleInput"), placeHandles);
     console.log("Places imported, see below.");
     console.log(places);
 }
+
+//--------Auto Complete Input
+
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
+          currentFocus++;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
+          currentFocus--;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          e.preventDefault();
+          if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+  }
